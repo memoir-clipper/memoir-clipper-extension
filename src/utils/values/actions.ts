b2de8e3214ebbf2b92a9ab093d3dca49bfd200bb
Jeel Patel <itsjeel01@gmail.com>
2025-07-02 15:20:22 +0530

    Add action type constants for extension messaging and toolbar operations

---
+++ src/utils/values/actions.ts
@@ -0,0 +1,7 @@
+/**
+ * Action type constants for extension messaging and toolbar operations.
+ */
+export const ACTION_GET_SELECTED_TEXT = 'getSelectedText';
+export const ACTION_TOOLBAR_SAVE = 'toolbarSave';
+export const ACTION_TOOLBAR_COPY = 'toolbarCopy';
+export const ACTION_TOOLBAR_CLOSE = 'toolbarClose';
